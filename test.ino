6f5fad5c8ab2941defc5672839bf2fb18bfb9184
arditxh1 <33842680+arditxh1@users.noreply.github.com>
2019-02-23 15:50:28 +0100

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,72 @@
+#define ledPin 3 
+int numri = 5;
+double timeRef = 0;
+String text = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop(){
+  if(Serial.available() > 0) {
+      text = text + Serial.readString();
+    }
+    respond();
+}
+
+void respond() {
+  if(text != ""){
+    Serial.println(text);
+    if(text == "ON")
+     {
+       digitalWrite(ledPin,HIGH); 
+     } 
+       else if(text == "OFF") 
+     {
+        digitalWrite(ledPin,LOW);
+      } 
+        if(text == "PARTY"){
+          for loop( i=3; i<0; i--){
+        digitalWrite(ledPin,HIGH);
+        delay(100);
+        digitalWrite(ledPin,LOW);
+        delay(100);
+     }
+  }
+  text = ""; 
+}
+  
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    
+
